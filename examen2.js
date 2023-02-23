1f7141a6713118734c411809a18d9512e74e178e
Veronicact1 <reinaveronicarich@gmail.com>
2023-02-23 21:06:22 +0100

    commit

--- examen2.js
+++ examen2.js
@@ -1,49 +1,55 @@
-// let n=prompt("Bienvenido a nuestra escuela");
-// console.log(n);
-// document.write(n);
-//ej.7
-// let text="Hola soy";
-// var y="Veronica";
-// console.log(n+""+text+""+y);
-//ej.8
-// var a=3;
-// if(a%2==0){
-//     console.log("el numero"+a+"es par");
-// }
-// else{
-//     console.log("el numero"+a+"es impar");
-// }
-//ej.9
-// let l=prompt("Escribe un numero entero positivo");
-// if ( l < 20) {
-//     console.log(l+1);
-// }
-// else {
-//     console.log(l);
-// }
-//ej.10
-// let palindromo="sometemos";
-// let inverso=""
-// console.log(palindromo.charAt(palindromo.length-1));
-// console.log(palindromo.length-1);
-// for (let k=palindromo.length-1; k=>0; k--)
-// {
-//  inverso += palindromo.charAt(k);
-//  console.log(inverso);
-// };
-// if (palindromo == inverso)
-// {
-// console.log("La variable palindromo: " +palindromo+ "es un palindromo")
-// }
-// else{
-//     console.log("La variable palindromo: " +palindromo+ " no es un palindromo")}
-    /*ej.11*/
-    // let colores=["yellow","green","red"];
-    // let repeat_colores=[];
-    // for (let y of colores){
-    // }
-    // repeat_colores.push(colores);
-    // console.log(repeat_colores);
+ /*ejercicio 6*/
+ let n=prompt("Bienvenido a nuestra escuela");
+ console.log(n);
+ document.write(n);
+/*ejercicio.7*/
+var y=n;
+ console.log('Hola soy'+" "+y);
+
+/*ejercicio.8*/
+ var a=3;
+ if(a%2==0){
+     console.log("el numero "+ a +" es par");
+ }
+ else{
+     console.log("el numero "+a+" es impar");
+}
+/* ejercicio 9*/
+let l=prompt('Escribe un numero entero positivo');
+for (let k=l; k<=20; k++)
+if (k<=20){
+   console.log(k)
+}
+else (k>20);{
+   console.log(l)
+}
+
+/*ej.10*/
+function texte()
+{
+ let palindromo="sometemos";
+ for (let p=0; p<palindromo.length; p++){
+ if (palindromo[p]!=palindromo[palindromo.length-p-1]){
+ return false;
+  }
+ }
+ return true;
+}
+ if(texte())
+ {console.log("La variable es palindromo");
+ }
+ else{
+   console.log("La variable no es palidromo")
+ }
+
+/*ej.11*/
+     let colores=["yellow","green","red"];
+     let repeat_colores=[];
+     for ( f of colores) {
+      repeat_colores.push(f);
+     }
+     console.log(repeat_colores);
+     
  
  /*ejercicio12
 1.Formamos un nuevo file.typescript
