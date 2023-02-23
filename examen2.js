437961a202dc61b6351b5b8df5fbb01c23143a12
Veronica Ceban <123936150+Veronicact1@users.noreply.github.com>
2023-02-23 12:01:42 +0100

    archivos

---
+++ examen2.js
@@ -0,0 +1,49 @@
+// let n=prompt("Bienvenido a nuestra escuela");
+// console.log(n);
+// document.write(n);
+//ej.7
+// let text="Hola soy";
+// var y="Veronica";
+// console.log(n+""+text+""+y);
+//ej.8
+// var a=3;
+// if(a%2==0){
+//     console.log("el numero"+a+"es par");
+// }
+// else{
+//     console.log("el numero"+a+"es impar");
+// }
+//ej.9
+let l=prompt("Escribe un numero entero positivo");
+if ( l < 20) {
+    console.log(l+1);
+}
+else {
+    console.log(l);
+}
+//ej.10
+// let palindromo="sometemos";
+// let inverso=""
+// console.log(palindromo.charAt(palindromo.length-1));
+// console.log(palindromo.length-1);
+// for (let k=palindromo.length-1; k=>0; k--)
+// {
+//  inverso += palindromo.charAt(k);
+//  console.log(inverso);
+// };
+// if (palindromo == inverso)
+// {
+// console.log("La variable palindromo: " +palindromo+ "es un palindromo")
+// }
+// else{
+//     console.log("La variable palindromo: " +palindromo+ " no es un palindromo")}
+    //ej.11
+    // let colores=["yellow","green","red"];
+    // let repeat_colores=[];
+    // for (let y of colores){
+    // }
+    // repeat_colores.push(colores);
+    // console.log(repeat_colores);
+
+    
+    
